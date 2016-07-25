0b9384b77ac00de3c864d9a4af5a9de5456fff1e
Lae0 <liaoxizhou_iphone@foxmail.com>
2016-07-25 16:56:42 +0800

    1、更新lae使用文档。2、增加安卓打包教程。

---
+++ librarys/android/example/LaeApp/app/src/main/java/com/example/lae/laeapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lae.laeapp;
+
+import android.os.Bundle;
+import com.lxzengine.androidapp.LXZEngineActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends LXZEngineActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
